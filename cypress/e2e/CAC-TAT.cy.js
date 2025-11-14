5ca98acfcf364b8e6892b96aee0db01d8d9df546
Artur Dantas Rodrigues <artur.dantas@compasso.com.br>
2025-11-13 22:01:05 -0300

    feat: add initial Cypress configuration and test suite for TAT application

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,86 @@
+describe("Central de Atendimento ao Cliente TAT", () => {
+  beforeEach(() => {
+    cy.visit("../../src/index.html");
+  });
+  it("verifica o título da aplicação", () => {
+    cy.title().should("equal", "Central de Atendimento ao Cliente TAT");
+  });
+
+  it("preenche campos obrigatórios e envia o formulário", () => {
+    cy.get('input[id="firstName"]').type("Artur", { delay: 0 });
+    cy.get('input[id="lastName"]').type("Dantas QA", { delay: 0 });
+    cy.get('input[id="email"]')
+      .should("be.visible")
+      .type("arturdantasrodrigues@gmail.com", {
+        delay: 0,
+      });
+    cy.get('input[id="phone"').type("83981914051", { delay: 0 });
+
+    cy.get('textarea[id="open-text-area"]').type(
+      "Boa tarde! Talvez vocês pudessem colocar uma cor mais comum no formulário, não acho que o rosa combine com a proposta da página.",
+      { delay: 0 }
+    );
+
+    cy.contains('button', 'Enviar').click();
+
+    cy.get(".success")
+      .should("be.visible")
+      .should("contain", "Mensagem enviada com sucesso.");
+  });
+
+  it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
+    cy.contains('button', 'Enviar').click();
+    cy.get(".error")
+      .should("be.visible")
+      .should("contain", "Valide os campos obrigatórios!");
+  });
+
+  it("não permite que o usuário digite valores não númericos no campo de telefone", () => {
+    const phone = cy.get('input[id="phone"').type("artur dantas", { delay: 0 });
+
+    phone.should("not.have.value");
+  });
+  it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
+    cy.get('input[id="firstName"]').type("Artur", { delay: 0 });
+    cy.get('input[id="lastName"]').type("Dantas QA", { delay: 0 });
+    cy.get('input[id="email"]')
+      .should("be.visible")
+      .type("arturdantasrodrigues@gmail.com", {
+        delay: 0,
+      });
+
+    cy.get("#phone-checkbox").click();
+
+    cy.get('textarea[id="open-text-area"]').type(
+      "Boa tarde! Talvez vocês pudessem colocar uma cor mais comum no formulário, não acho que o rosa combine com a proposta da página.",
+      { delay: 0 }
+    );
+
+    cy.contains('button', 'Enviar').click();
+
+    cy.get(".error")
+      .should("be.visible")
+      .should("contain", "Valide os campos obrigatórios!");
+  });
+
+  it("preenche e limpa os campos nome, sobrenome, email e telefone", () => {
+    cy.get('input[id="firstName"]').type("Artur", { delay: 0 }).clear().should('not.have.value');
+    cy.get('input[id="lastName"]').type("Dantas QA", { delay: 0 }).clear().should('not.have.value');
+    cy.get('input[id="email"]')
+      .should("be.visible")
+      .type("arturdantasrodrigues@gmail.com", {
+        delay: 0,
+      }).clear().should('not.have.value');
+    cy.get('input[id="phone"').type("83981914051", { delay: 0 }).clear().should('not.have.value');
+
+    cy.get('textarea[id="open-text-area"]').type(
+      "Boa tarde! Talvez vocês pudessem colocar uma cor mais comum no formulário, não acho que o rosa combine com a proposta da página.",
+      { delay: 0 }
+    ).clear().should('not.have.value');
+  });
+
+  it('envia o formulário com sucesso usando comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit({firstName: 'Testando coisas'})
+  }) 
+
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,57 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', { prevSubject: 'optional'}, (subject, options = {}) => {
+    const { 
+        firstName = 'Artur', 
+        lastName = 'Dantas QA', 
+        email = 'arturdantasrodrigues@gmail.com', 
+        phone = '83981914051', 
+        feedback = 'Isso aqui é o negócio' 
+    } = options;
+    
+    const root = subject || cy;
+
+    root.get('input[id="firstName"]').type(firstName, { delay: 0 });
+    root.get('input[id="lastName"]').type(lastName, { delay: 0 });
+    root.get('input[id="email"]')
+      .should("be.visible")
+      .type(email, {
+        delay: 0,
+      });
+    root.get('input[id="phone"]').type(phone, { delay: 0 });
+
+    root.get('textarea[id="open-text-area"]').type(
+      feedback,
+      { delay: 0 }
+    );
+
+    root.contains('button', 'Enviar').click();
+
+    root.get(".success")
+      .should("be.visible")
+      .should("contain", "Mensagem enviada com sucesso.");
+})
