fa95b9bd2f431f3357b64df5c667e331bc880ab1
Artur Dantas Rodrigues <artur.dantas@compasso.com.br>
2026-01-06 14:14:53 -0300

    Refactor code structure for improved readability and maintainability

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.js'
+  },
+  video: true
 })
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -49,7 +49,7 @@ describe("Central de Atendimento ao Cliente TAT", () => {
         delay: 0,
       });
 
-    cy.get("#phone-checkbox").click();
+    cy.get("#phone-checkbox").check();
 
     cy.get('textarea[id="open-text-area"]').type(
       "Boa tarde! Talvez vocês pudessem colocar uma cor mais comum no formulário, não acho que o rosa combine com a proposta da página.",
@@ -120,7 +120,7 @@ describe("Central de Atendimento ao Cliente TAT", () => {
       .should("be.checked");
   });
 
-  it.only("marca cada tipo de atendimento - minha solução", () => {
+  it("marca cada tipo de atendimento - minha solução", () => {
     cy.get('input[value="ajuda"][type="radio"]').check().should("be.checked");
     cy.get('input[value="elogio"][type="radio"]').check().should("be.checked");
     cy.get('input[value="feedback"][type="radio"]')
@@ -128,9 +128,73 @@ describe("Central de Atendimento ao Cliente TAT", () => {
       .should("be.checked");
   });
 
-  it.skip("marca cada tipo de atendimento - solucação do professor", () => {
+  it.skip("marca cada tipo de atendimento - solução do professor", () => {
     cy.get('[type="radio"]').each((typeOfService) => {
       cy.wrap(typeOfService).check().should("be.checked");
     });
+  });
+
+  it("marca ambos checkboxes, depois desmarca o último", () => {
+    cy.get('input[type="checkbox"]').as("checkboxes").check();
+
+    cy.get("@checkboxes")
+      .each((contactType) => {
+        cy.wrap(contactType).should("be.checked");
+      })
+      .last()
+      .uncheck()
+      .should("not.be.checked");
+  });
+
+  it("seleciona um arquivo da pasta fixtures", () => {
+    cy.get('input[type="file"').as("file");
+
+    cy.get("@file").selectFile("cypress/fixtures/example.json");
+
+    cy.get("@file").then((file) => {
+      expect(file[0].files[0].name).to.be.equal("example.json");
+    });
+  });
+
+  it("seleciona um arquivo simulando um drag-and-drop", () => {
+    cy.get('input[type="file"').as("file");
+
+    cy.get("@file").selectFile("cypress/fixtures/example.json", {
+      action: "drag-drop",
+    });
+
+    cy.get("@file").then((file) => {
+      expect(file[0].files[0].name).to.be.equal("example.json");
+    });
+  });
+
+  it("seleiona um arquivo utilizando uma fixture para a qual foi dada um alias", () => {
+    cy.get('input[type="file"').as("file");
+    cy.fixture("example.json").as("example-file");
+
+    cy.get("@file").selectFile("@example-file");
+
+    cy.get("@file").then((file) => {
+      expect(file[0].files[0].name).to.be.equal("example.json");
+    });
+  });
+
+  it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", () => {
+    cy.contains("a", "Política de Privacidade").should(
+      "have.attr",
+      "target",
+      "_blank"
+    );
+  });
+
+  it("acessa a página da política de privacidade removendo o target e então clicando no link", () => {
+    cy.contains("a", "Política de Privacidade")
+      .invoke("removeAttr", "target")
+      .click();
+
+    cy.title().should(
+      "equal",
+      "Central de Atendimento ao Cliente TAT - Política de Privacidade"
+    );
   });
 });
