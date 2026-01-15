7eaeab8808b0ec94d8d28e3b5ace7309d1c5ee5a
Artur Dantas Rodrigues <artur.dantas@compasso.com.br>
2026-01-15 16:19:47 -0300

    refactor: improve test assertions and add missing story in fixture

--- cypress/e2e/hardcoded-assertion/sample.cy.js
+++ cypress/e2e/hardcoded-assertion/sample.cy.js
@@ -1,27 +1,21 @@
-describe('Hardcoded assertion bad practice', () => {
+import { hits } from '../../fixtures/stories.json'
+
+describe("Hardcoded assertion bad practice", () => {
   beforeEach(() => {
-    cy.intercept(
-      'GET',
-      '**/search**',
-      { fixture: 'stories' }
-    ).as('getStories')
+    cy.intercept("GET", "**/search**", { fixture: "stories" }).as("getStories");
 
-    cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
-  })
+    cy.visit("https://hackernews-seven.vercel.app");
+    cy.wait("@getStories");
+  });
 
-  it('searches', () => {
-    cy.search('cypress.io')
-    cy.wait('@getStories')
+  it("searches", function () {
+    cy.search("cypress.io");
+    cy.wait("@getStories");
 
-    cy.get('.table-row')
-      .as('tableRows')
-      .should('have.length', 2)
-    cy.get('@tableRows')
-      .eq(0)
-      .should('contain', 'Agile Testing')
-    cy.get('@tableRows')
-      .eq(1)
-      .should('contain', 'Clean Code')
-  })
-})
+    cy.get(".table-row").as("tableRows").should("have.length", hits.length);
+
+    hits.forEach((story, index) => {
+      cy.get("@tableRows").eq(index).should("contain", story.title);
+    });
+  });
+});
