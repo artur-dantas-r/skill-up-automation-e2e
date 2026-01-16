563ee14e8580feea742fe2fdf11e8915a338fff5
Artur Dantas Rodrigues <artur.dantas@compasso.com.br>
2026-01-16 09:33:39 -0300

    refactor: replace page object method with direct command call in sample.cy.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,19 +1,24 @@
-Cypress.Commands.add('search', (term, hitEnter = true) => {
+Cypress.Commands.add("search", (term, hitEnter = true) => {
   cy.get('input[type="text"]')
-      .should("be.visible")
-      .clear()
-      .type(`${term}${hitEnter ? "{enter}" : ""}`);
-})
+    .should("be.visible")
+    .clear()
+    .type(`${term}${hitEnter ? "{enter}" : ""}`);
+});
 
-Cypress.Commands.add('assertResults', () => {
-  cy.get('.table-row').then(rows => {
-    expect(rows.length).to.be.at.least(1)
-  })
-})
+Cypress.Commands.add("assertResults", () => {
+  cy.get(".table-row").then((rows) => {
+    expect(rows.length).to.be.at.least(1);
+  });
+});
 
-Cypress.Commands.add('randomlyTogglePurchaseAgreement', () => {
+Cypress.Commands.add("updateInfo", (data) => {
+  cy.get("#destination_name").clear().type(data.name);
+  cy.get("#destination_description").clear().type(data.description);
+  cy.get('input[type="submit"]').click();
+});
+
+Cypress.Commands.add("randomlyTogglePurchaseAgreement", () => {
   if (Math.random() > 0.5) {
-    cy.get('#agree')
-      .click()
+    cy.get("#agree").click();
   }
-})
+});
