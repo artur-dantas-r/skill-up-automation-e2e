ee59d9364ce5552b4259ba2afbd782143be68dd8
Artur Dantas Rodrigues <artur.dantas@compasso.com.br>
2026-04-07 18:42:45 -0300

    Print screen when test fail.

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -183,7 +183,7 @@ export const config = {
 	mochaOpts: {
 		ui: 'bdd',
 		timeout: 60000
-	}
+	},
 
 	//
 	// =====
@@ -279,8 +279,11 @@ export const config = {
 	 * @param {boolean} result.passed    true if test has passed, otherwise false
 	 * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
 	 */
-	// afterTest: function(test, context, { error, result, duration, passed, retries }) {
-	// },
+	afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+		if (error) {
+        	await driver.takeScreenshot();
+    	}
+	},
 
 	/**
 	 * Hook that gets executed after the suite has ended
